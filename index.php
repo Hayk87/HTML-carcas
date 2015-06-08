8e9165f7b1f3a6d3318256aaf7d1366257ca53de
Hayk Dallakyan <dallakyanhayk@mail.ru>
2015-06-08 10:35:35 +0400

    html_carcas

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>site Title</title>
+	</head>
+	<link rel="stylesheet" type="text/css" href="css/jqueryui.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<body>
+		
+		html body is here!
+		
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/jqueryui.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/functions.js"></script>
+	</body>
+</html>
